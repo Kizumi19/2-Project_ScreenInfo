b29cd1163dab4880a1daa1aa710eddbf9b1bfcfb
Kizumi19 <juliakbgamer12@gmail.com>
2023-10-28 09:56:07 +0200

    Remove unnecessari index.php redirection

--- public/index.php
+++ public/index.php
@@ -3,12 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-if ($_SERVER['REQUEST_URI'] == '/') {
-    header('Location: /index');
-    exit();
-}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-
