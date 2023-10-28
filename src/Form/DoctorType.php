c5d98dbb5732668454d7653cab2d601bba7f3eec
Kizumi19 <juliakbgamer12@gmail.com>
2023-10-28 12:24:07 +0200

    Improve doctor's CRUD

--- src/Form/DoctorType.php
+++ src/Form/DoctorType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Doctor;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,22 @@ class DoctorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('hidden')
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Nom',
+                    'required' => true,
+                ]
+            )
+            ->add(
+                'surname',
+                TextType::class,
+                [
+                    'label' => 'Cognoms',
+                    'required' => true,
+                ]
+            )
         ;
     }
 
