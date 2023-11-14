c491bac3eaaac518d9f9f8a5b1e98778ccbcfd6d
Kizumi19 <juliakbgamer12@gmail.com>
2023-11-14 11:16:44 +0100

    Update Admin menu and Location can read id Shift

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -17,8 +17,12 @@ class LocationController extends AbstractController
     #[Route('/', name: 'app_location_index', methods: ['GET'])]
     public function index(LocationRepository $locationRepository): Response
     {
+        $locations = $locationRepository->findAll();
+
+
         return $this->render('location/index.html.twig', [
             'locations' => $locationRepository->findAll(),
+
         ]);
     }
 
