62a4596bde9d1a14893dfa6e44c54e48d5992fc3
Kizumi19 <juliakbgamer12@gmail.com>
2023-11-29 09:47:11 +0100

    Fix registration and login backend

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -21,20 +21,16 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Hash (encriptar) la contraseña
             $hashedPassword = $passwordHasher->hashPassword(
                 $user,
                 $form->get('password')->getData()
             );
             $user->setPassword($hashedPassword);
 
-            // Guardar usuario
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // Aquí deberías redirigir al usuario a la página de inicio de sesión,
-            // o donde sea apropiado después de un registro exitoso.
-            return $this->redirectToRoute('app_login'); // Asegúrate de que esta ruta exista
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
