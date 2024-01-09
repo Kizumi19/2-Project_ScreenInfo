369514ef5cccc0c4aaea960894fc341249c9dcaa
Kizumi19 <juliakbgamer12@gmail.com>
2024-01-09 10:44:08 +0100

    Add manual download

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class InfoController extends AbstractController
 {
@@ -14,5 +15,12 @@ class InfoController extends AbstractController
         return $this->render('info/index.html.twig', [
             'controller_name' => 'InfoController',
         ]);
+    }
+    #[Route('/manual', name: 'download_manual')]
+    public function downloadManual(): Response
+    {
+        $pdfPath = $this->getParameter('kernel.project_dir') . '/public/files/Manual_Del_UsuariFinal_EKRANAS.pdf';
+
+        return new BinaryFileResponse($pdfPath);
     }
 }
